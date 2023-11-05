7351c5613d71705390dda29b9e90879e805c2658
Yassef AyubBoggeano <ayub.yassef@icloud.com>
2023-11-04 19:17:33 -0700

    more app routes added

--- helloWorld.js
+++ helloWorld.js
@@ -1,12 +1,7 @@
 var express=require('express');
 var app=express();
-var port = 5000;
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
-app.listen(port, () => {
-    console.log('Server is running on port ${port}');
-});
+// var port = 5000;
+
 app.route('/Node').get(function(req,res){
     res.send("Testing my Node Skills");
 });
@@ -15,6 +10,22 @@ app.route('/Angular').get(function(req, res){
         res.send("Check Out My Angular Skills");
 });
 
-app.get('/', function (req, res){
-    res.send('I taught myself this. :-)');
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.get('/about', function (req, res){
+    res.send('This is the about page');
+});
+
+app.get('/contact', function (req, res){
+    res.send('Contact me at emailme@email.com');
+});
+
+app.get('*', function (req, res){
+    res.send('I taught mysself this, btw... :-)');
+});
+
+app.listen(port, () => {
+    console.log('Server is running on port ${port}');
 });
